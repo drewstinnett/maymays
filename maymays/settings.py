e01cb63d4282d2a9ad4ab70b8a1916ab3f4ee4d0
drews@duke.edu <drews@duke.edu>
2019-01-19 19:56:16 -0500

    Changing setting

--- maymays/settings.py
+++ maymays/settings.py
@@ -17,6 +17,8 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+THUMBNAIL_FORCE_OVERWRITE = True
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/')
 
